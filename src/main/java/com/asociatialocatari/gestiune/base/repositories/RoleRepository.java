2667e6ceb2e6edc215251f1f8db3e9f102daedde
ggsaiux <ggsaiu@gmail.com>
2023-01-18 22:53:15 +0100

    Initial commit

---
+++ src/main/java/com/asociatialocatari/gestiune/base/repositories/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.asociatialocatari.gestiune.base.repositories;
+
+import java.util.Optional;
+import com.asociatialocatari.gestiune.base.models.ERole;
+import com.asociatialocatari.gestiune.base.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
+}
---
+++ src/main/java/com/asociatialocatari/gestiune/base/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.asociatialocatari.gestiune.base.repositories;
+
+import com.asociatialocatari.gestiune.base.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+}
